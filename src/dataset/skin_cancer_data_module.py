297eef093f0f4477c458c0f4756b5bff4ec11bb6
EstefanoB <estefanobvflu10@gmail.com>
2022-10-30 20:00:24 -0300

    Remove shuffling from validation and testing

--- src/dataset/skin_cancer_data_module.py
+++ src/dataset/skin_cancer_data_module.py
@@ -97,7 +97,7 @@ class SkinCancerDataModule(pl.LightningDataModule):
     def val_dataloader(self) -> DataLoader:
         return DataLoader(
             self.validation,
-            shuffle=True,
+            shuffle=False,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
         )
@@ -105,7 +105,7 @@ class SkinCancerDataModule(pl.LightningDataModule):
     def test_dataloader(self) -> DataLoader:
         return DataLoader(
             self.test,
-            shuffle=True,
+            shuffle=False,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
         )
