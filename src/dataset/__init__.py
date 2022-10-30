71d65c1c19bfe6ef7525ed468c3e4a9ee211580d
EstefanoB <estefanobvflu10@gmail.com>
2022-10-30 13:05:50 -0300

    Create metadata functions

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -2,8 +2,9 @@
 Python modules to manipulate the dataset.
 """
 
+from . import metadata
 from .plot_samples import plot_some_samples
 from .skin_cancer_data_module import SkinCancerDataModule
 from .skin_cancer_dataset import SkinCancerDataset
 
-__all__ = ["SkinCancerDataset", "SkinCancerDataModule", "plot_some_samples"]
+__all__ = ["SkinCancerDataset", "SkinCancerDataModule", "plot_some_samples", "metadata"]
