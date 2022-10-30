29e0dfac6c9ad2b635be53960874c95e41d0fc62
EstefanoB <estefanobvflu10@gmail.com>
2022-10-30 12:11:51 -0300

    Move plot some samples to a new file

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -2,7 +2,8 @@
 Python modules to manipulate the dataset.
 """
 
+from .plot_samples import plot_some_samples
 from .skin_cancer_data_module import SkinCancerDataModule
 from .skin_cancer_dataset import SkinCancerDataset
 
-__all__ = ["SkinCancerDataset", "SkinCancerDataModule"]
+__all__ = ["SkinCancerDataset", "SkinCancerDataModule", "plot_some_samples"]
