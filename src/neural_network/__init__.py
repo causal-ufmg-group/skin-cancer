03e9efdae9a04dc5b7f39962cdc255ad40fcb7c2
EstefanoB <estefanobvflu10@gmail.com>
2022-10-29 21:37:43 -0300

    Create neural network init file

---
+++ src/neural_network/__init__.py
@@ -0,0 +1,8 @@
+"""
+Python modules to define the neural network and lightning module.
+"""
+
+from .conv_network import ConvNetwork
+from .network_module import NetworkModule
+
+__all__ = ["ConvNetwork", "NetworkModule"]
