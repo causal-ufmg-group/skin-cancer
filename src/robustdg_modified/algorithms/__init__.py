29668b189338153962d435fac8af512e98e9d7f7
EstefanoB <estefanobvflu10@gmail.com>
2022-11-27 23:22:38 -0300

    Ad __init__.py

---
+++ src/robustdg_modified/algorithms/__init__.py
@@ -0,0 +1,9 @@
+"""
+Necessary algorithms from robustdg.
+"""
+
+from .erm_match import ErmMatch
+from .hybrid import Hybrid
+from .match_dg import MatchDG
+
+__all__ = ["ErmMatch", "Hybrid", "MatchDG"]
