443f5bb173f8d165c13581921575dfec5c827b02
EstefanoB <estefanobvflu10@gmail.com>
2022-12-06 13:28:51 -0300

    Move models to robustdg_modified

--- src/robustdg_modified/models/__init__.py
+++ src/robustdg_modified/models/__init__.py
@@ -3,6 +3,5 @@ Python modules to define the neural network and lightning module.
 """
 
 from .conv_network import ConvNetwork
-from .network_module import NetworkModule
 
-__all__ = ["ConvNetwork", "NetworkModule"]
+__all__ = ["ConvNetwork"]
