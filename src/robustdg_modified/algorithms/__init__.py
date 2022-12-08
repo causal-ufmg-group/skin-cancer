027f5d16c3cd4943cbdcc5d985b106fef6008bb0
EstefanoB <estefanobvflu10@gmail.com>
2022-12-08 17:27:11 -0300

    Add function to describe best method

--- src/robustdg_modified/algorithms/__init__.py
+++ src/robustdg_modified/algorithms/__init__.py
@@ -6,5 +6,6 @@ from .erm_match import ErmMatch
 from .hybrid import Hybrid
 from .match_dg import MatchDG
 from .no_domain import NoDomain
+from .utils.best_method import get_best_method
 
-__all__ = ["ErmMatch", "Hybrid", "MatchDG", "NoDomain"]
+__all__ = ["ErmMatch", "Hybrid", "MatchDG", "NoDomain", "get_best_method"]
