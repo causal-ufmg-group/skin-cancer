850eaa5c6280483824e1ef4fd03dd7c2a9a6bb74
EstefanoB <estefanobvflu10@gmail.com>
2022-12-11 17:35:15 -0300

    Add InceptionV3

--- src/robustdg_modified/models/__init__.py
+++ src/robustdg_modified/models/__init__.py
@@ -5,6 +5,13 @@ Python modules to define the neural network and lightning module.
 from . import utils
 from .conv_network import ConvNetwork
 from .pre_trained_densenet import PreTrainedDenseNet121
+from .pre_trained_inceptionv3 import PreTrainedInceptionV3
 from .pre_trained_resnet import PreTrainedResNet18
 
-__all__ = ["utils", "ConvNetwork", "PreTrainedResNet18", "PreTrainedDenseNet121"]
+__all__ = [
+    "utils",
+    "ConvNetwork",
+    "PreTrainedDenseNet121",
+    "PreTrainedResNet18",
+    "PreTrainedInceptionV3",
+]
